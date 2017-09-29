fe6d1520dc69febc35bd11fc3ff113f26e491b6a
zstudent <zstudent>
2017-09-29 17:13:51 +0300

    first commit

--- lambda/src/test/java/lambda/part1/example/Lambdas01.java
+++ lambda/src/test/java/lambda/part1/example/Lambdas01.java
@@ -117,7 +117,6 @@ public class Lambdas01 {
                 new Person("name 2", "lastName 1", 30)
         );
 
-        
         final Map<String, Person> personByLastName =
                 FluentIterable.from(persons)
                         .uniqueIndex(new Function<Person, String>() {
